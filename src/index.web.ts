5779efa38c703a168134aeff6500a2c62ee7e49f
Kim CHOUARD <github@chouard.kim>
2024-02-12 15:52:58 +0100

    [🧪 failed] CanvasKitJS tentative

--- src/index.web.ts
+++ src/index.web.ts
@@ -1,12 +1,22 @@
+// This file should only import and register the root. No components or exports
+// should be added here.
+
 import '@expo/metro-runtime';
-import { LoadSkiaWeb } from '@shopify/react-native-skia/lib/module/web';
 import { App } from 'expo-router/build/qualified-entry';
 import { renderRootComponent } from 'expo-router/build/renderRootComponent';
 
-// This file should only import and register the root. No components or exports
-// should be added here.
+// RN Skia (WASM)
+import { LoadSkiaWeb } from '@shopify/react-native-skia/lib/module/web';
+
 LoadSkiaWeb({
   locateFile: (file) => `/public/static/js/${file}`,
 }).then(async () => {
   renderRootComponent(App);
 });
+
+// RN Skia (CanvasKitJS)
+// import { CanvasKitJS } from 'canvaskit-js';
+// (async () => {
+//   global.CanvasKit = CanvasKitJS.getInstance();
+//   renderRootComponent(App);
+// })();
